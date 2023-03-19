120f354a073a6c35c889ba94b1acd0261b2c3ad0
vincenttommi <vincenttommi@gmail.com>
2023-03-19 21:49:49 +0300

    reveiwing conditionals in js

---
+++ index.js
@@ -0,0 +1,29 @@
+// const age = 20;
+
+
+// let isAdult;
+
+
+// if(age >= 18){
+
+//     isAdult = true;
+// }
+
+
+// console.log(isAdult);
+
+
+// 
+
+const age = 50;
+
+let isAdult;
+
+if(age >= 80 ){
+
+
+isAdult = true;
+
+}
+
+console.log(isAdult);
