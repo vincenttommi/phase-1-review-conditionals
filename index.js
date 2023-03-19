64236b7d76ae229d346a71a8f6798326ea6d60cf
vincenttommi <vincenttommi@gmail.com>
2023-03-19 22:22:47 +0300

    nested if statements in javascript

--- index.js
+++ index.js
@@ -1,84 +1,114 @@
+// // const age = 20;
+
+
+// // let isAdult;
+
+
+// // if(age >= 18){
+
+// //     isAdult = true;
+// // }
+
+
+// // console.log(isAdult);
+
+
+// // 
+
+// // const age = 50;
+
+// // let isAdult;
+
+// // if(age >= 80 ){
+
+
+// // isAdult = true;
+
+// // }
+
+// // console.log(isAdult);
+
+
+// //using tenary expressions
+
+// // const age = 30;
+
+// // let isAdult;
+
+// // age >= 18 ? (isAdult = true) : (isAdult = false);
+
+// // console.log(isAdult);
+
+// //Remember that a ternary is an expression 
+
+
+
+// // const age = 26;
+// // const  isAdult = age >= 18 ? true : false;
+
+// // console.log(isAdult);
+
+
+// // else if statemens
+
 // const age = 20;
 
+// let isAdult, canWork, canEnlist, canDrink;
 
-// let isAdult;
-
-
-// if(age >= 18){
-
+// if (age >= 21) {
 //     isAdult = true;
+//     canWork = true;
+//     canEnlist = true;
+//     canDrink = true;
+// } else if (age >= 18) {
+//     isAdult = true;
+//     canWork = true;
+//     canEnlist = true;
+// } else if (age >= 16) {
+//     canWork = true;
 // }
+// // => true
+
+// isAdult;
+// // => true
+
+// canWork;
+// // => true
+
+// canEnlist;
+// // => true
+
+// canDrink;
+
+// // => undefined
+// console.log(isAdult,canEnlist,canEnlist,canDrink);
 
 
-// console.log(isAdult);
+// Nested if statements
 
 
-// 
+const age = 17;
 
-// const age = 50;
+let isAdult,canWork,canEnlist, canDrink;
 
-// let isAdult;
+if(age >= 16){
 
-// if(age >= 80 ){
-
-
-// isAdult = true;
-
-// }
-
-// console.log(isAdult);
+    canWork = true;
 
 
-//using tenary expressions
+    if(age >= 18){
 
-// const age = 30;
-
-// let isAdult;
-
-// age >= 18 ? (isAdult = true) : (isAdult = false);
-
-// console.log(isAdult);
-
-//Remember that a ternary is an expression 
-
-
-
-// const age = 26;
-// const  isAdult = age >= 18 ? true : false;
-
-// console.log(isAdult);
-
-
-// else if statemens
-
-const age = 20;
-
-let isAdult, canWork, canEnlist, canDrink;
-
-if (age >= 21) {
     isAdult = true;
-    canWork = true;
     canEnlist = true;
-    canDrink = true;
-} else if (age >= 18) {
-    isAdult = true;
-    canWork = true;
-    canEnlist = true;
-} else if (age >= 16) {
-    canWork = true;
+
+    if(age >= 21){
+
+
+        canDrink = true;
+    }
+
+    }
+
 }
-// => true
 
-isAdult;
-// => true
-
-canWork;
-// => true
-
-canEnlist;
-// => true
-
-canDrink;
-
-// => undefined
-console.log(isAdult,canEnlist,canEnlist,canDrink);
+console.log(canWork);
